3e48a6e881f3be43c55196e251c0f6a9bfa4fdbf
JT <jstorbit@gmail.com>
2026-02-07 14:27:27 -0700

    Add seed logic for users and update playlist logic

---
+++ db/queries/users.js
@@ -0,0 +1,15 @@
+import db from '#db/client';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+
+export const createUser = async ({username, password}) => {
+  const sql = `
+    INSERT INTO users (username, password)
+    VALUES ($1, $2)
+    RETURNING username
+  `;
+  const securePassword = await bcrypt.hash(password, 10);
+  const { rows: [user] } = await db.query(sql, [username, securePassword])
+  const token = jwt.sign({ user }, process.env.JWT_SECRET);
+  return token;
+}
--- db/seed.js
+++ db/seed.js
@@ -3,6 +3,7 @@ import db from "#db/client";
 import { createPlaylist } from "#db/queries/playlists";
 import { createPlaylistTrack } from "#db/queries/playlists_tracks";
 import { createTrack } from "#db/queries/tracks";
+import { createUser } from "./queries/users.js";
 
 await db.connect();
 await seed();
@@ -10,8 +11,17 @@ await db.end();
 console.log("🌱 Database seeded.");
 
 async function seed() {
+  for (let i = 1; i <= 3; i++) {
+    await createUser({ username: `seedUser${i}`, password: `${i}password` });
+  }
   for (let i = 1; i <= 20; i++) {
-    await createPlaylist("Playlist " + i, "lorem ipsum playlist description");
+    let userId = null;
+    if (i % 2 === 1) {
+      userId = 1;
+    } else {
+      userId = 2;
+    }
+    await createPlaylist("Playlist " + i, "lorem ipsum playlist description", userId);
     await createTrack("Track " + i, i * 50000);
   }
   for (let i = 1; i <= 15; i++) {
